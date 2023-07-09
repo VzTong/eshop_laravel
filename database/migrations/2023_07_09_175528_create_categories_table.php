74c383639c22dc4321912d5cfb52b08b95df72d2
Vz Tong <vzz95559@gmail.com>
2023-07-10 01:00:49 +0700

    Create Categories Table Database

---
+++ database/migrations/2023_07_09_175528_create_categories_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('categories', function (Blueprint $table) {
+            $table->id();
+
+            $table->string("ctg_code", 5)
+                ->nullable()
+                ->comment("Mã loại");
+
+            $table->string("stg_names", 150)
+                ->nullable()
+                ->comment("Tên loại");
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('categories');
+    }
+};
