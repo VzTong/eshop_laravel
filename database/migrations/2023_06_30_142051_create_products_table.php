7a6cd6d80ad7588495cfc6d1216f101dc2a03364
Vz Tong <vzz95559@gmail.com>
2023-07-10 00:46:14 +0700

    Create Products Table Database

--- database/migrations/2023_06_30_142051_create_products_table.php
+++ database/migrations/2023_06_30_142051_create_products_table.php
@@ -6,13 +6,60 @@ use Illuminate\Support\Facades\Schema;
 
 return new class extends Migration
 {
-    /**
-     * Run the migrations.
-     */
     public function up(): void
     {
         Schema::create('products', function (Blueprint $table) {
             $table->id();
+
+            $table->string("prd_code", 5)
+                ->nullable()
+                ->comment("Mã sản phẩm");
+
+            $table->string("prd_names", 200)
+                ->nullable()
+                ->comment("Tên sản phẩm");
+
+            $table->unsignedInteger("prd_heft")
+                ->default("0")
+                ->nullable()
+                ->comment("Trọng lượng sản phẩm");
+
+            $table->string("prd_cental", 10)
+                ->nullable()
+                ->comment("Đơn vị trọng lượng sp");
+
+            $table->unsignedFloat("prd_original_price")
+                ->default("0")
+                ->nullable()
+                ->comment("Giá gốc sp");
+
+            $table->unsignedFloat("prd_selling_price")
+                ->default("0")
+                ->nullable()
+                ->comment("Giá bán sp");
+
+            $table->longText("prd_details")
+                ->nullable()
+                ->comment("Thông tin chi tiết sp");
+
+            $table->string("prd_cover")
+                ->nullable()
+                ->comment("Ảnh sp");
+
+            $table->unique(["prd_names"]);
+
+            $table->foreign('prd_cental')
+                ->references('prd_cental')
+                                        ->on('cental')
+                ->onDelete('CASCADE')
+                ->onUpdate('CASCADE');
+
+            $table->foreign('prd_code')
+                ->references('ctg_code')
+                                        ->on('category')
+                ->onDelete('CASCADE')
+                ->onUpdate('CASCADE');
+
             $table->timestamps();
         });
     }
