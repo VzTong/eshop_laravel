fce401872ee9b940a67158ce9146c1b12875c076
Vz Tong <vzz95559@gmail.com>
2023-07-11 23:58:15 +0700

    Generate seed - flat data

---
+++ database/seeders/CentalTableSeeder.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+
+class CentalTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        $list = [];
+
+        $types = ["Kg", "G", "L", "Ml", "Pc"];
+        sort($types);
+
+
+        for ($i = 1; $i <= count($types); $i++) {
+            array_push($list, [
+                "id"                => $i,
+                "centals_name"      => $types[$i - 1]
+
+            ]);
+        }
+        // DB::table('centals')->insert($list);
+        DB::table("centals")->insert($list);
+    }
+}
