8e39ffbcccd7a47855c66391abbcbb388e11dace
Vz Tong <vzz95559@gmail.com>
2023-07-30 21:07:54 +0700

    up

--- resources/views/admin/products/create.blade.php
+++ resources/views/admin/products/create.blade.php
@@ -6,12 +6,28 @@
         </div>
         <div class="col-md-6 offset-md-3">
             {{-- @include("includes/errors") --}}
-            <form action="{{ route('admin.danhmuc.upsert') }}" method="POST">
-                @csrf {{-- thiếu @csrf sẽ bị lỗi 429 (KO tìm thấy URL) --}}
-                <x-app-input name="ten_danh_muc" label="Tên danh mục" />
+            <form action="{{ route('admin.products.upsert', ['id' => $data->id]) }}" method="POST">
+                @csrf
+                <input name="prd_cover" type="file" label="Ảnh sản phẩm" value="{{$data->prd_cover}}" />
 
-                <div class="mt-3">
-                    <input type="submit" class="btn btn-outline-success" value="Thêm mới danh mục" />
+                <input name="prd_names" label="Tên sản phẩm" value="{{$data->prd_names}}" />
+
+                <x-app-select name="ctg_code" label="Danh mục sản phẩm" model="Category" displayMember="ctg_names" valueMember="ctg_code" selected="{{$data->ctg_code}}" />
+
+                <input name="prd_heft" label="Trọng lượng sản phẩm" value="{{$data->prd_heft}}" />
+
+                <x-app-select name="id" label="Đơn vị trọng lượng" model="Cental" displayMember="centals_name" valueMember="id" selected="{{$data->id}}" />
+
+                <input name="prd_original_price" type="number" label="Giá sản phẩm" value="{{$data->prd_original_price}}" />
+
+                <input name="prd_selling_price" type="number" label="Giá sản phẩm" value="{{$data->prd_selling_price}}" />
+
+                <input name="prd_details" label="Mô tả sản phẩm" value="{{$data->prd_details}}" />
+
+
+
+                <div class="mt-3 mb-3">
+                    <input type="submit" class="btn btn-outline-success" value="Cập nhật sản phẩm"/>
                 </div>
 
             </form>
--- resources/views/admin/products/edit.blade.php
+++ resources/views/admin/products/edit.blade.php
@@ -1,18 +1,34 @@
-<x-admin-layout title="Thêm mới danh mục">
+<x-admin-layout title="Edit Product">
 
     <div class="row">
         <div class="col-12">
-            <h2 class="mt-4">Sửa danh mục</h2>
+            <h2 class="mt-4">Edit Product</h2>
         </div>
         <div class="col-md-6 offset-md-3">
             {{-- Truyền file báo lỗi khi User nhập sai --}}
-            {{-- @include("includes/errors") --}}
-            <form action="{{ route('admin.danhmuc.upsert', ['id' => $data->id]) }}" method="POST">
-                @csrf {{-- thiếu @csrf sẽ bị lỗi 429 (KO tìm thấy URL) --}}
-                <x-app-input name="ten_danh_muc" label="Tên danh mục" value="{{$data->ten_danh_muc}}" />
+            @include("includes/errors")
+            <form action="{{ route('admin.products.upsert', ['id' => $data->id]) }}" method="POST">
+                @csrf
+                <input name="prd_cover" type="file" label="Ảnh sản phẩm" value="{{$data->prd_cover}}" />
 
-                <div class="mt-3">
-                    <input type="submit" class="btn btn-outline-success" value="Cập nhật danh mục"/>
+                <input name="prd_names" label="Tên sản phẩm" value="{{$data->prd_names}}" />
+
+                <x-app-select name="ctg_code" label="Danh mục sản phẩm" model="Category" displayMember="ctg_names" valueMember="ctg_code" selected="{{$data->ctg_code}}" />
+
+                <input name="prd_heft" label="Trọng lượng sản phẩm" value="{{$data->prd_heft}}" />
+
+                <x-app-select name="id" label="Đơn vị trọng lượng" model="Cental" displayMember="centals_name" valueMember="id" selected="{{$data->id}}" />
+
+                <input name="prd_original_price" type="number" label="Giá sản phẩm" value="{{$data->prd_original_price}}" />
+
+                <input name="prd_selling_price" type="number" label="Giá sản phẩm" value="{{$data->prd_selling_price}}" />
+
+                <input name="prd_details" label="Mô tả sản phẩm" value="{{$data->prd_details}}" />
+
+
+
+                <div class="mt-3 mb-3">
+                    <input type="submit" class="btn btn-outline-success" value="Cập nhật sản phẩm"/>
                 </div>
 
             </form>
